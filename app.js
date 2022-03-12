a6856dd1d96c97df34839265251fb0bb23eb43e4
fesuarezj <fesuarezj@gmail.com>
2022-03-12 17:22:50 -0500

    Agregando Estilos el proyecto Mercado Liebre, Segundo Desafio: Estilos

--- app.js
+++ app.js
@@ -3,13 +3,16 @@ const path = require("path");
 const app = express()
 const port = 3030
 
+//Se declara la ruta de los recursos estaticos del proyecto: /public
 const publicPath = path.resolve(__dirname, './public');
 app.use( express.static(publicPath) );
 
+//Se inicia el servidor express en el puerto definido
 app.listen(port, ()=> {
     console.log(`Servidor Express ejecutando Ok en el puerto : ${port}`);
 });
 
+//Bloque donde se definen las Rutas del proyecto
 app.get('/', (req, res) => {
     res.sendFile(path.resolve(__dirname, './views/home.html'));
 });
